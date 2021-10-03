6e12ec3d153ef70ed3c04f19ba6283407199f6a5
Ryan-ops68 <72042413+Ryan-ops68@users.noreply.github.com>
2021-10-03 12:19:34 -0400

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import csv 
+import plotly.graph_objects as go
+df = pd.read_csv("data.csv")
+student_df = df.loc[df["student_id"]=="TRL_xsl"]
+fig = go.Figure(go.Scatter(x = student_df.groupby("level")["attempt"].mean(),y = ["level 1", "level 2", "level 3", "level 4"]))
+fig.show()
